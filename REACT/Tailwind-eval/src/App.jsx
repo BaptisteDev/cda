fbd745a4a4bd481a257edc530e0c22ca864b55dc
BaptisteDev <dev.baptiste15@gmail.com>
2025-03-28 13:26:00 +0100

    up

--- REACT/Tailwind-eval/src/App.jsx
+++ REACT/Tailwind-eval/src/App.jsx
@@ -1,9 +1,14 @@
 // import "./App.css";
 
+import Header from "./components/layouts/Header";
+import Content from "./components/content/Content";
+import Footer from "./components/layouts/Footer";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Tailwind</h1>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
     </>
   );
 }
